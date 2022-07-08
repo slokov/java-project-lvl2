9202b39929b613cff49d363feabda93ea0d7cc2e
Pavel Mironov <pavel.mironov@sbermarket.ru>
2022-07-08 18:21:31 +0300

    add pinocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+
+import picocli.CommandLine;
+
 public class App {
     public static void main(String[] args) {
+
+        int exitCode = new CommandLine(new Differ()).execute(args);
+        System.exit(exitCode);
         System.out.println("Hello World!");
+
     }
 }
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "Differ", mixinStandardHelpOptions = true, version = "differ 0.1",
+        description = "Compares two configuration files and shows a difference.\n")
+
+public class Differ {
+    static String generate() {
+        return "";
+    }
+}
